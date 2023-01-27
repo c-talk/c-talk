1fb965078b26e9b3d9741e8a58d1374fd5b5502f
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-01-27 15:16:38 +0800

    chore: init

---
+++ src/pages/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>Default Page</h1>
+    <p>This is the default page.</p>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,137 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import Preview from 'vite-plugin-vue-component-preview'
+import Vue from '@vitejs/plugin-vue'
+import VueJSX from '@vitejs/plugin-vue-jsx'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Markdown from 'vite-plugin-vue-markdown'
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
+import Inspect from 'vite-plugin-inspect'
+import Inspector from 'vite-plugin-vue-inspector'
+import LinkAttributes from 'markdown-it-link-attributes'
+import Unocss from '@unocss/vite'
+import Shiki from 'markdown-it-shiki'
+import VueMacros from 'unplugin-vue-macros/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Preview(),
+
+    VueMacros({
+      plugins: {
+        vue: Vue({
+          include: [/\.vue$/, /\.md$/],
+          reactivityTransform: true
+        }),
+        vueJsx: VueJSX()
+      }
+    }),
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      dirs: [{ dir: resolve(__dirname, 'src/pages'), baseRoute: '' }],
+      extensions: ['vue', 'md']
+    }),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts({
+      layoutsDirs: [resolve(__dirname, 'src/layouts')]
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core'
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores'],
+      vueTemplate: true
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts'
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+
+    // https://github.com/antfu/vite-plugin-vue-markdown
+    Markdown({
+      wrapperClasses: 'prose prose-sm m-auto text-left',
+      headEnabled: true,
+      markdownItSetup(md) {
+        md.use(Shiki, {
+          theme: {
+            light: 'github-dark',
+            dark: 'github-light'
+          }
+        })
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener'
+          }
+        })
+      }
+    }),
+
+    // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
+    VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      fullInstall: true,
+      include: [resolve(__dirname, 'src/locales/**')]
+    }),
+
+    // https://github.com/antfu/vite-plugin-inspect
+    // Visit http://localhost:3333/__inspect/ to see the inspector
+    Inspect(),
+
+    // https://github.com/webfansplz/vite-plugin-vue-inspector
+    Inspector({
+      toggleButtonVisibility: 'never'
+    })
+  ],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG
+  },
+  resolve: {
+    alias: [
+      { find: /^@(?=\/)/, replacement: resolve(__dirname, './src') },
+      { find: /^~(?=\/)/, replacement: resolve(__dirname, './') }
+    ]
+  }
+})
